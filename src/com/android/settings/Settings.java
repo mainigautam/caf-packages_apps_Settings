3aeff54eb2ccc44fa5008826f312d85aa78e56ca
inderpreetsinghb <inderpreetbhangu2@gmail.com>
2019-10-31 18:29:29 +0000

    Add OneCustoms dashboard

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -178,5 +178,6 @@ public class Settings extends SettingsActivity {
     public static class SystemDashboardActivity extends SettingsActivity {}
     public static class SupportDashboardActivity extends SettingsActivity {}
     public static class SMQQtiFeedbackActivity extends SettingsActivity { /* empty */ }
+    public static class OneSettingsActivity extends SettingsActivity {}
 
 }
